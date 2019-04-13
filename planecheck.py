f6246aaf881c97e88d232f8d02a1193a48a15177
Doug Ip <dougip@gmail.com>
2019-04-13 13:45:54 +0200

    test

--- planecheck.py
+++ planecheck.py
@@ -6,5 +6,5 @@ page = requests.get('http://www.planecheck.com/aspsel2.asp?man=Beech&page=0')
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
-mylist = soup.find_all('a')
+mylist = soup.find_all('div')
 print mylist[0]
