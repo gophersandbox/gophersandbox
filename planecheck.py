6bf09afab87b17256bc3f2addf48960635c9f03c
Doug Ip <dougip@gmail.com>
2019-04-13 13:05:28 +0200

    test

--- planecheck.py
+++ planecheck.py
@@ -8,6 +8,6 @@ soup = BeautifulSoup(page.content, 'html.parser')
 print soup
 
 
-#mylist = soup.find(class_='info')
-#for element in mylist:
-#    print element
+#mylist = soup.find_all('table')
+for element in mylist:
+    print element.contents
