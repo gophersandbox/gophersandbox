ae6c488ee97ba85291de119ea599f1c57cbc7960
Doug Ip <dougip@gmail.com>
2019-04-13 12:32:02 +0200

    test

---
+++ planecheck.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('http://www.planecheck.com/aspsel2.asp?man=Beech&page=0')
+
+soup = BeautifulSoup(page.content)
+
+print soup
+
+
+#mylist = soup.find(class_='info')
+#for element in mylist:
+#    print element
