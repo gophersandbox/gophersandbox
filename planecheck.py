efe25d2fceeb8fab2e43af6a47ca5f68dadbca0b
Doug Ip <dougip@gmail.com>
2019-04-17 00:04:53 +0200

    test

--- planecheck.py
+++ planecheck.py
@@ -3,13 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser()
-parser.add_argument("environment")
+parser.add_argument('environment')
 args = parser.parse_args()
 
-if args.echo == 'production':
+if args.environment == 'production':
   page = requests.get('http://www.planecheck.com/aspsel2.asp?man=Beech&page=0')
   soup = BeautifulSoup(page.content, 'html.parser')
-elif args.echo == 'development':
+elif args.environment == 'development':
   page = open('test/testpage.html')
   soup = BeautifulSoup(page.content, 'html.parser')
   page.close()
